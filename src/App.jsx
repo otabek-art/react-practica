06174c597ce678e6e5872d8b09441c80dc7d1c6c
otabek-art <pocani497@gmail.com>
2024-11-12 20:57:50 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.scss'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+
+
+
+const App = () => {
+  return (
+<>
+<Header/>
+<Hero num= '1' text= 'HELLO WORLD'  />
+<Hero num= '2'  />
+<Hero num= '3'  />
+<Hero num= '4'  />
+<Hero num= '5'  />
+
+</>
+  )
+}
+
+export default App
---
+++ src/components/Aa/Aa.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Aa.scss'
+
+const Aa = ( {num} ) => {
+  return (
+ <>
+     <a href={`#hero${num}`}>SECTION {num} </a>
+ </>
+  )
+}
+
+export default Aa
